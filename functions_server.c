b3d13979ee71e55e0a3b92d7413bccf4e4eb7414
Flavio <titticchio@gmail.com>
2025-06-20 22:34:50 +0200

    spostata qualche funzione del server in functions_server

--- functions_server.c
+++ functions_server.c
@@ -1,5 +1,50 @@
 #include "header_server.h"
 
+void init_socket(int port, int *server_fd, int *client_fd)
+{
+    struct sockaddr_in addr;
+    socklen_t addr_len = sizeof(addr);
+
+    // 1. Crea socket
+    *server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*server_fd == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    // 2. Imposta indirizzo locale
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY; // Ascolta su tutte le interfacce
+    addr.sin_port = htons(port);
+
+    // 3. Bind
+    if (bind(*server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        close(*server_fd);
+        exit(1);
+    }
+    // 4. Listen
+    if (listen(*server_fd, 1) < 0)
+    {
+        perror("listen");
+        close(*server_fd);
+        exit(1);
+    }
+
+    printf("[SERVER] In ascolto su porta %d...\n", PORT);
+
+    // 5. Accept
+    *client_fd = accept(*server_fd, (struct sockaddr *)&addr, &addr_len);
+    if (*client_fd < 0)
+    {
+        perror("accept");
+        close(*server_fd);
+        exit(1);
+    }
+}
+
 char *receive_msg(int client_fd)
 {
     char buffer[BUF_SIZE];
--- server.c
+++ server.c
@@ -4,48 +4,7 @@
 int main()
 {
     int server_fd, client_fd;
-    struct sockaddr_in addr;
-    socklen_t addr_len = sizeof(addr);
-
-    // 1. Crea socket
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
-        perror("socket");
-        exit(1);
-    }
-
-    // 2. Imposta indirizzo locale
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY; // Ascolta su tutte le interfacce
-    addr.sin_port = htons(PORT);
-
-    // 3. Bind
-    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        perror("bind");
-        close(server_fd);
-        exit(1);
-    }
-
-    // 4. Listen
-    if (listen(server_fd, 1) < 0)
-    {
-        perror("listen");
-        close(server_fd);
-        exit(1);
-    }
-
-    printf("[SERVER] In ascolto su porta %d...\n", PORT);
-
-    // 5. Accept
-    client_fd = accept(server_fd, (struct sockaddr *)&addr, &addr_len);
-    if (client_fd < 0)
-    {
-        perror("accept");
-        close(server_fd);
-        exit(1);
-    }
+    init_socket(PORT, &server_fd, &client_fd);
 
     char *msg = receive_msg(client_fd);
     printf("[SERVER] Messaggio ricevuto: %s\n", msg);
